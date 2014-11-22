728e85f5d8e1d2d56e06bc5b0ca0de8a73ad9a48
Steven GERARD <steven.gerard.deruelle@gmail.com>
2014-11-21 21:56:44 -0500

    Le squelette peut aussi nous tuer maintenant.

---
+++ unity/Assets/AI/Actions/hitTarget.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class hitTarget : RAINAction
+{
+    public override void Start(RAIN.Core.AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+		GameObject target = ai.WorkingMemory.GetItem<GameObject>("varSkeleton");
+		Health hp = (Health)target.GetComponent("Health");
+		hp.CurrentHealth = hp.CurrentHealth - ai.WorkingMemory.GetItem<int>("damage");
+		
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
